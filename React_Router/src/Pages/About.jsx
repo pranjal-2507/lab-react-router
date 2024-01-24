08bdf811ac552ae24134fd6a257bc772e6e0eeb7
Pranjal <pranjalgosavi2507@gmail.com>
2024-01-24 16:57:11 +0530

    Done

---
+++ React_Router/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+function About() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Navbar /> About
+    </div>
+  );
+}
+
+export default About;
---
+++ React_Router/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
